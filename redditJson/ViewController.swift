c3b04a543385c62806fc49344d0a028138be306e
Skyler Tanner <tannerskyler@gmail.com>
2015-12-07 09:24:08 -0700

    Initial Commit

---
+++ redditJson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redditJson
+//
+//  Created by Skyler Tanner on 12/7/15.
+//  Copyright © 2015 Skyler Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
